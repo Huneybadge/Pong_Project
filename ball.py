80d7363de564ecbd949a66d78844975f8e946543
patikolo <116673510+patikolo@users.noreply.github.com>
2022-12-06 14:45:58 -0500

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  6 14:28:55 2022
+
+@author: patriciakolodziejski
+"""
+
+import pygame 
+from random import randint 
+
+bg_color = (0,0,0)
+
+class Ball(pygame.sprite.Sprite):
+    # This class makes the ball
+    
+    def __init__(self, color, width, height):
+        # Calling the parent class
+        super().__init__()
+         
+        # Getting the color of the ball.
+        # Setting the background.
+        self.image = pygame.Surface([width, height])
+        self.image.fill(bg_color)
+        self.image.set_color(bg_color)
+        
+        #Drawing the ball as a rectangle and getting starting velocity
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.velocity = [randint(4,8), randint(-8,8)]
+        
+        # Get the object with the dimensions of the image
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
