9e2542792951386521909683fced3249d5db52c2
Huneybadge <116671437+Huneybadge@users.noreply.github.com>
2022-12-12 14:52:19 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -30,3 +30,35 @@ class Ball(pygame.sprite.Sprite):
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = randint(-8, 8)
+
+class Speed_Ball(pygame.sprite.Sprite):
+    # This class makes the ball for the speed stack mode
+    
+    def __init__(self, color, width, height):
+        # Calling the parent class
+        super().__init__()
+        
+        # Getting the color of the ball and setting the background
+        self.image = pygame.Surface([width, height])
+        self.image.fill(bg_color)
+        self.image.set_colorkey(bg_color)
+        
+        # Drawing the ball as a rectangle and getting starting velocity
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.velocity = [4, randint(-3,3)]
+        
+        # Get the object with the dimensions of the image
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        
+    def bounce(self):
+        self.velocity[0] = -(self.velocity[0] + 5)
+        self.velocity[1] = randint(-8, 8)
+    
+    # def reset(self):
+    #     self.velocity[0] = 0
+    #     self.velocity[1] = 0
+        
